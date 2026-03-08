0fd8acd3bc743474d8276624505547866e17cdf9
srinijagidugu <srinija.gidugu@exponentialai.com>
2026-03-08 09:44:32 +0530

    even or odd, find unique num

---
+++ Practice/even_or_odd_without_modulus.py
@@ -0,0 +1,7 @@
+num = 8
+num2 = 1
+
+if num&num2 == 1:
+    print("ODD")
+else:
+    print("EVEN")
---
+++ Practice/find_unique_num.py
@@ -0,0 +1,7 @@
+l = [2, 4, 3, 6, 4, 9, 3, 2, 6]
+res = 0
+
+for i in l:
+    res ^= i
+
+print(res)
